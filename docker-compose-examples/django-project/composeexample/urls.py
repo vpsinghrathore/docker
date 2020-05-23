e0ed2d44d259cb815e2abc1266938ce0a542f942
vpsinghrathore <52044627+vpsinghrathore@users.noreply.github.com>
2020-05-23 19:00:18 +0530

    Update urls.py

--- docker-compose-examples/django-project/composeexample/urls.py
+++ docker-compose-examples/django-project/composeexample/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import url
+
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
 ]
